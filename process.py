8883746469fa642741c77af5cbfcfa2424c588a7
Madhav Datt <madhav.datt@hotmail.com>
2017-04-16 21:29:00 +0530

    complete preprocessing, wordcount

--- process.py
+++ process.py
@@ -7,10 +7,18 @@
 from pyspark import SparkContext, SparkConf
 from bs4 import BeautifulSoup
 from creole import creole2html
+from nltk.stem import SnowballStemmer
+from nltk.corpus import stopwords
+import re
+
+stop = set(stopwords.words('english'))
+stemmer = SnowballStemmer('english')
 
 
 class Article(object):
-
+    """
+    Class with wikipedia article instance
+    """
     def __init__(self, title, text, links=None):
         self.title = title
         self.text = text
@@ -18,19 +26,49 @@ class Article(object):
 
 
 def remove_html(article):
-    soup = BeautifulSoup(article.text, "lxml")
-    article.text = soup.get_text()
-    return article
+    title, text = article
+    soup = BeautifulSoup(text, "lxml")
+    text = soup.get_text()
+    return title, text
 
 
 def parse_xml(line):
     soup = BeautifulSoup(line, "lxml")
-    return Article(soup.page.title, soup.page.text)
+    # return soup.page.title.string
+    # return tuple([soup.page.title, soup.page.text])
+    try:
+        title = soup.page.title.string
+    except AttributeError:
+        title = ""
+    try:
+        text = soup.page.text
+    except AttributeError:
+        text = ""
+    return title, text
 
 
 def remove_creole(article):
-    article.text = creole2html(article.text)
-    return article
+    title, text = article
+    text = creole2html(text)
+    return title, text
+
+
+def pre_process(token):
+    """
+    Remove punctuation, numbers and stopwords from text tokens
+    :param token: token to be processed
+    :return: cleaned token string
+    """
+
+    token = ' ' + token.lower() + ' '
+    tmp_token = re.sub("[^a-zA-Z\s]", ' ', token)
+    for word in tmp_token.split():
+        replace_word = ' ' + str(word) + ' '
+        if word in stop:
+            tmp_token = tmp_token.replace(replace_word, ' ')
+        tmp_token = tmp_token.replace(replace_word, ' ' + stemmer.stem(word) + ' ')
+
+    return ' '.join(tmp_token.split())
 
 # Set up main entry point for Spark
 conf = (SparkConf()
@@ -42,9 +80,10 @@ sc.setLogLevel("OFF")
 
 print("\n\n\n")
 # directory = input("Data dump directory absolute path: ")
-directory = "/home/madhav/Downloads/DBMS"
+# directory = "/home/madhav/Downloads/DBMS"
 
-data_file = "file:///{path}/*.txt".format(path=directory)
+# data_file = "file:///{path}/*.txt".format(path=directory)
+data_file = 'test.txt'
 rdd_data = sc.textFile(data_file).cache()
 rdd_data_string = [rdd_data.reduce(lambda a, b: a + '\n' + b)]
 
@@ -56,11 +95,14 @@ rdd_xml = sc.parallelize(rdd_data_string, 16)\
         .map(remove_html)
 
 # Pre-processing and parsing out creole text
-rdd_counter = rdd_xml.map(creole2html).map(remove_html)
+# rdd_counter = rdd_xml.map(creole2html).map(remove_html)
+rdd_counter = rdd_xml.map(lambda article: article[1])\
+    .map(pre_process)
 
 counts = rdd_counter.flatMap(lambda line: line.split(" ")) \
     .map(lambda word: (word, 1)) \
     .reduceByKey(lambda a, b: a + b)
+
 print(counts.collect())
 # counts.saveAsTextFile("word-counts.txt")
 sc.stop()
