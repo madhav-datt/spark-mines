efb282a87f53fea45b6fa859f4bfd68be4b3206d
Madhav Datt <madhav.datt@hotmail.com>
2017-04-17 14:07:56 +0530

    add page rank link parse functions

--- process.py
+++ process.py
@@ -10,10 +10,10 @@ from creole import creole2html
 from nltk.stem.wordnet import WordNetLemmatizer
 from nltk.corpus import stopwords
 import re
+import pickle
 import io
 
 stop = set(stopwords.words('english'))
-# stemmer = SnowballStemmer('english')
 lem = WordNetLemmatizer()
 
 
@@ -21,6 +21,7 @@ class Article(object):
     """
     Class with wikipedia article instance
     """
+
     def __init__(self, title, text, links=None):
         self.title = title
         self.text = text
@@ -35,15 +36,15 @@ def remove_html(article):
 
 
 def parse_xml(line):
-    line = re.sub('<sha1(.)*?>(.)*?</sha1>',' ',line,count=0,flags=re.DOTALL)
-    line = re.sub('<username(.)*?>(.)*?</username>',' ',line,count=0,flags=re.DOTALL)
-    line = re.sub('<id(.)*?>(.)*?</id>',' ',line,count=0,flags=re.DOTALL)
-    line = re.sub('<model(.)*?>(.)*?</model>',' ',line,count=0,flags=re.DOTALL)
-    line = re.sub('<timestamp(.)*?>(.)*?</timestamp>',' ',line,count=0,flags=re.DOTALL)
-    line = re.sub('<format(.)*?>(.)*?</format>',' ',line,count=0,flags=re.DOTALL)
-    line = re.sub('<comment(.)*?>(.)*?</comment>',' ',line,count=0,flags=re.DOTALL)
-
+    line = re.sub('<sha1(.)*?>(.)*?</sha1>', ' ', line, count=0, flags=re.DOTALL)
+    line = re.sub('<username(.)*?>(.)*?</username>', ' ', line, count=0, flags=re.DOTALL)
+    line = re.sub('<id(.)*?>(.)*?</id>', ' ', line, count=0, flags=re.DOTALL)
+    line = re.sub('<model(.)*?>(.)*?</model>', ' ', line, count=0, flags=re.DOTALL)
+    line = re.sub('<timestamp(.)*?>(.)*?</timestamp>', ' ', line, count=0, flags=re.DOTALL)
+    line = re.sub('<format(.)*?>(.)*?</format>', ' ', line, count=0, flags=re.DOTALL)
+    line = re.sub('<comment(.)*?>(.)*?</comment>', ' ', line, count=0, flags=re.DOTALL)
     soup = BeautifulSoup(line, "lxml")
+
     # return soup.page.title.string
     # return tuple([soup.page.title, soup.page.text])
     try:
@@ -63,6 +64,20 @@ def remove_creole(article):
     return title, text
 
 
+def parse_links(article):
+    pattern = re.compile("\[\[([^\]]*)\]\]")
+    title, text = article
+    links = pattern.findall(text)
+
+    for i in range(len(links)):
+        if '|' in links[i]:
+            links[i] = links[i][:links[i].index('|')]
+        if '#' in links[i]:
+            links[i] = links[i][:links[i].index('#')]
+
+    return title, links
+
+
 def pre_process(token):
     """
     Remove punctuation, numbers and stopwords from text tokens
@@ -73,6 +88,7 @@ def pre_process(token):
     token = re.sub('[^\sa-zA-Z]', ' ', token)  # ''.join(e for e in token if e.isalnum())
     token = token.lower()
     return ' '.join([word for word in token.split() if word not in stop])
+
 
 # Set up main entry point for Spark
 conf = (SparkConf()
@@ -91,16 +107,23 @@ data_file = 'data2.txt'
 rdd_data = sc.textFile(data_file).cache()
 rdd_data_string = [rdd_data.reduce(lambda a, b: a + '\n' + b)]
 
-rdd_xml = sc.parallelize(rdd_data_string, 16)\
-        .flatMap(lambda line: line.split('</page>'))\
-        .map(lambda line: line + '</page>')\
-        .map(parse_xml)\
-        .map(remove_creole)\
-        .map(remove_html)
+rdd_xml = sc.parallelize(rdd_data_string, 16) \
+    .flatMap(lambda line: line.split('</page>')) \
+    .map(lambda line: line + '</page>') \
+    .map(parse_xml)
+
+# Parse links from text into separate dictionary
+rdd_links = rdd_xml.map(parse_links)
+links_dict = rdd_links.collectAsMap()
+with io.open('pagerank.pickle', 'wb') as handle:
+    pickle.dump(links_dict, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+rdd_xml = rdd_xml.map(remove_creole) \
+    .map(remove_html)
 
 # Pre-processing and parsing out creole text
 # rdd_counter = rdd_xml.map(creole2html).map(remove_html)
-rdd_counter = rdd_xml.map(lambda article: article[1])\
+rdd_counter = rdd_xml.map(lambda article: article[1]) \
     .map(pre_process)
 
 rdd_title = rdd_xml.map(lambda article: article[0])
